8e1c10d7609c245e66efd6c6998995a659e0c7b4
xuyiqing88 <xu-yi-qing@163.com>
2025-07-18 15:03:22 +0800

    app icon

---
+++ sw.js
@@ -0,0 +1,20 @@
+// sw.js
+const CACHE_NAME = 'design-tools-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  // 添加其他需要缓存的资源
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+});
