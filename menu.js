60013162f87d91f2c819ffdbd1643f27dc2a0182
xuyiqing88 <xu-yi-qing@163.com>
2025-09-22 10:48:01 +0800

    update

--- menu.js
+++ menu.js
@@ -32,7 +32,6 @@ body {
     width: 1em;
     height: 1em;
     fill: currentColor;
-    margin-right: 4px;
 }
 .icon2 {
     display: inline-block;
@@ -327,6 +326,7 @@ body {
             font-size: 0.9rem;
             display: flex;
         align-items: center;
+        gap: 4px;
         }
 
         .content-wrapper {
